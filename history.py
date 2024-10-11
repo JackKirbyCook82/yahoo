49d9b2faeec6f0504cbbdeeb40c596fee314012c
JackKirbyCook82 <jackcookchee@yahoo.com>
2024-10-11 05:58:26 -0500

    Updated 1/10/2024

--- history.py
+++ history.py
@@ -11,11 +11,11 @@ import numpy as np
 import pandas as pd
 from datetime import datetime as Datetime
 
-from finance.variables import Variables, Symbol
+from finance.variables import Variables, Querys
 from webscraping.webpages import WebBrowserPage
 from webscraping.webdatas import WebHTML
 from webscraping.weburl import WebURL
-from support.mixins import Emptying, Sizing, Logging, Pipelining
+from support.mixins import Emptying, Sizing, Logging, Pipelining, Sourcing
 from support.meta import RegistryMeta
 
 __version__ = "1.0.0"
@@ -37,19 +37,6 @@ class YahooHistoryParsers(object):
         dataframe = dataframe.rename(columns={"adj": "price"}, inplace=False)
         dataframe = dataframe[~dataframe["open"].apply(str).str.contains("Dividend") & ~dataframe["open"].apply(str).str.contains("Split")]
         return dataframe
-
-
-class YahooHistoryVariables(object):
-    axes = {Variables.Querys.HISTORY: ["date", "ticker"], Variables.Querys.SYMBOL: ["ticker"]}
-    data = {Variables.Technicals.BARS: ["price", "open", "close", "high", "low", "volume"]}
-
-    def __init__(self, *args, **kwargs):
-        self.history = self.axes[Variables.Querys.HISTORY]
-        self.symbol = self.axes[Variables.Querys.SYMBOL]
-        self.bars = self.data[Variables.Technicals.BARS]
-        self.index = self.history
-        self.columns = self.bars
-        self.header = self.index + self.columns
 
 
 class YahooTechnicalURL(WebURL):
@@ -88,16 +75,14 @@ class YahooBarsPage(YahooTechnicalPage, register=Variables.Technicals.BARS):
         return dataframe
 
 
-class YahooTechnicalDownloader(Pipelining, Sizing, Emptying, Logging):
+class YahooTechnicalDownloader(Pipelining, Sourcing, Sizing, Emptying, Logging):
     def __init__(self, *args, technical=Variables.Technicals.BARS, **kwargs):
         Pipelining.__init__(self, *args, **kwargs)
         Logging.__init__(self, *args, **kwargs)
-        self.__variables = YahooHistoryVariables(*args, techncial=technical, **kwargs)
         self.__page = YahooTechnicalPage[technical](*args, **kwargs)
-        self.__technical = technical
 
     def execute(self, symbols, *args, dates, **kwargs):
-        for symbol in self.symbols(symbols):
+        for symbol in self.source(symbols, Querys.Symbol):
             parameters = dict(ticker=symbol.ticker, dates=dates)
             bars = self.download(*args, **parameters, **kwargs)
             size = self.size(bars)
@@ -111,17 +96,6 @@ class YahooTechnicalDownloader(Pipelining, Sizing, Emptying, Logging):
         assert isinstance(bars, pd.DataFrame)
         return bars
 
-    @staticmethod
-    def symbols(symbols):
-        assert isinstance(symbols, (list, Symbol))
-        assert all([isinstance(symbol, Symbol) for symbol in symbols]) if isinstance(symbols, list) else True
-        symbols = [symbols] if not isinstance(symbols, list) else symbols
-        yield from iter(symbols)
-
-    @property
-    def variables(self): return self.__variables
-    @property
-    def technical(self): return self.__technical
     @property
     def pages(self): return self.__pages
 
